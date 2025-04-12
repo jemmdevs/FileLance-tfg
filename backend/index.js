eae4911ec65f720cbb3cc2d33c6309e7080ae61a
jemmdevs <jemmdev@gmail.com>
2025-04-12 17:24:28 +0200

    seventh and last try

--- backend/index.js
+++ backend/index.js
@@ -14,7 +14,7 @@ app.use('/', router);
 
 const __dirname = path.resolve();
 app.use(express.static(path.join(__dirname, './frontend/dist')));
-app.get('*', (req, res) => 
+app.get('/*', (req, res) => 
     res.sendFile(path.join(__dirname, "frontend", "dist", "index.html")));
 
 app.listen(PORT, () => 
